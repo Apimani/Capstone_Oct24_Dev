669482c8b71a2893e0833671b27262295d7d8b53
Hetu <sahu.hetram@gmail.com>
2024-12-09 08:52:09 +0530

    tranform completed

--- Scripts/extract.py
+++ Scripts/extract.py
@@ -3,10 +3,14 @@ from sqlalchemy import create_engine
 import cx_Oracle
 
 # Create mysql engine
-mysql_engine = create_engine('mysql+pymysql://root:Admin%40143@localhost:3308/retaildwh')
+#mysql_engine = create_engine('mysql+pymysql://root:Admin%40143@localhost:3308/retaildwh')
+from Scripts.config import *
+
+mysql_engine = create_engine(f'mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}')
 
 # Create Oracle engine
-oracle_engine = create_engine('oracle+cx_oracle://system:admin@localhost:1521/xe')
+#oracle_engine = create_engine('oracle+cx_oracle://system:admin@localhost:1521/xe')
+oracle_engine = create_engine(f'oracle+cx_oracle://{ORACLE_USER}:{ORACLE_PASSWORD}@{ORACLE_HOST}:{ORACLE_PORT}/{ORACLE_SERVICE}')
 
 def extract_sales_dataSRC_Load_STG():
     df = pd.read_csv("data/sales_data.csv")
@@ -25,6 +29,11 @@ def extract_inventory_dataSRC_Load_STG():
     df = pd.read_xml("data/inventory_data.xml",xpath=".//item")
     df.to_sql("staging_inventory",mysql_engine,if_exists='replace',index=False)
 
+def extract_stores_data_OracleSRC_Load_STG():
+    query =  """select * from stores"""
+    df = pd.read_sql(query,oracle_engine)
+    df.to_sql("staging_stores",mysql_engine,if_exists='replace',index=False)
+
 
 if __name__== '__main__':
     print("Data Extrcation strted ....")
@@ -32,5 +41,6 @@ if __name__== '__main__':
     extract_product_dataSRC_Load_STG()
     extract_inventory_dataSRC_Load_STG()
     extract_supplier_dataSRC_Load_STG()
+    extract_stores_data_OracleSRC_Load_STG()
     print("Data Extrcation completed ....")
 
